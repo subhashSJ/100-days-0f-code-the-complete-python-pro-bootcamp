5f4f05003347972edfc897e8236f4026d11739cb
subhashSJ <jadhavsubhash511@gmail.com>
2023-11-03 12:33:43 +0530

    Day 1: worked with Python variables and functions (print, input, len)

---
+++ Day 1 - Working with variables in python to manage data/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator.")
+
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet name?\n")
+
+print("Your band name could be: " + city_name + " " + pet_name)
